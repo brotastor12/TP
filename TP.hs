386fe7b7a33f24723ac669586944e2f3c2a94844
ahri10 <87216166+ahri10@users.noreply.github.com>
2023-05-08 11:37:35 -0300

    Add files via upload

---
+++ TP.hs
@@ -0,0 +1,35 @@
+type Usuario = (Integer, String) -- (id, nombre)
+type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
+type Publicacion = (Usuario, String, [Usuario]) -- (usuario que publica, texto publicacion, likes)
+type RedSocial = ([Usuario], [Relacion], [Publicacion])
+
+-- Funciones basicas
+
+usuarios :: RedSocial -> [Usuario]
+usuarios (us, _, _) = us
+
+relaciones :: RedSocial -> [Relacion]
+relaciones (_, rs, _) = rs
+
+publicaciones :: RedSocial -> [Publicacion]
+publicaciones (_, _, ps) = ps
+
+idDeUsuario :: Usuario -> Integer
+idDeUsuario (id, _) = id 
+
+nombreDeUsuario :: Usuario -> String
+nombreDeUsuario (_, nombre) = nombre 
+
+usuarioDePublicacion :: Publicacion -> Usuario
+usuarioDePublicacion (u, _, _) = u
+
+likesDePublicacion :: Publicacion -> [Usuario]
+likesDePublicacion (_, _, us) = us
+
+-- Predicados 
+
+pertenece :: Eq t => t -> [t] -> Bool
+pertenece x ys 
+        | ys == [] = False
+        | x == head ys = True 
+        | x /= head ys = (pertenece x (tail ys)) 
