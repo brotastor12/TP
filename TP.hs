f226f5bb9828e0b77d1f3bf55e3b1070b0cdab1f
brotastor12 <49822601+brotastor12@users.noreply.github.com>
2023-05-11 11:19:39 -0300

    Update TP.hs

--- TP.hs
+++ TP.hs
@@ -6,27 +6,35 @@ type RedSocial = ([Usuario], [Relacion], [Publicacion])
 -- Funciones basicas
 usuarios :: RedSocial -> [Usuario]
 usuarios (us, _, _) = us
+
 relaciones :: RedSocial -> [Relacion]
 relaciones (_, rs, _) = rs
+
 publicaciones :: RedSocial -> [Publicacion]
 publicaciones (_, _, ps) = ps
+
 idDeUsuario :: Usuario -> Integer
 idDeUsuario (id, _) = id 
+
 nombreDeUsuario :: Usuario -> String
 nombreDeUsuario (_, nombre) = nombre 
+
 usuarioDePublicacion :: Publicacion -> Usuario
 usuarioDePublicacion (u, _, _) = u
+
 likesDePublicacion :: Publicacion -> [Usuario]
 likesDePublicacion (_, _, us) = us
 
 -- Ejercicios
 nombresDeUsuarios :: RedSocial -> [String]
 nombresDeUsuarios red = proyectarNombres (usuarios red)
+
 proyectarNombres :: [Usuario] -> [String]
 proyectarNombres us | sinRepetidos listaNombres = listaNombres
                     | otherwise = sacarRepetidos listaNombres
                     where listaNombres = proyectarNombresAux us
 -- proyectarNombres revisa si hay repetidos
+
 proyectarNombresAux :: [Usuario] -> [String]
 proyectarNombresAux [] = []
 proyectarNombresAux (x:xs) = nombreDeUsuario x : proyectarNombres xs
@@ -40,26 +48,34 @@ pertenece _ [] = False
 pertenece x (y:ys)
         | x == y = True 
         | otherwise = pertenece x ys
+
 usuarioValido :: Usuario -> Bool
 usuarioValido u = idDeUsuario u > 0 && (length (nombreDeUsuario u)) > 0
 -- Indica si una lista contiene los mismos elementos sin importar el orden ni las repeticiones              
+
 mismoselementos :: Eq t => [t] -> [t] -> Bool
 mismoselementos l1 l2 = incluido l1 l2 && incluido l2 l1
 -- Provee el elemento final de una lista             
+
 terminacon :: [t] -> t
 terminacon xs = head (reverse xs)
 -- Verifica que tiene id y que tiene nombre 
+
 usuarioValido :: Usuario -> Bool
 usuarioValido u = idDeUsuario u > 0 && (length (nombreDeUsuario u)) > 0
+
 incluido :: Eq t => [t] -> [t] -> Bool
 incluido (x:l1) l2 | pertenece x l2 = incluido l1 l2
                    | otherwise = False
+
 sinRepetidos :: Eq t => [t] -> Bool
 sinRepetidos [] = True
 sinRepetidos (x:xs) | pertenece x xs = False
                     | otherwise = sinRepetidos xs
+
 sonDeLaRed :: RedSocial -> [Usuario] -> Bool
 sonDeLaRed red us = incluido us (usuarios red)
+
 sacarRepetidos :: Eq a => [a] -> [a]
 sacarRepetidos [] = []
 sacarRepetidos (x:xs) | pertenece x xs = sacarRepetidos xs
@@ -68,6 +84,7 @@ sacarRepetidos (x:xs) | pertenece x xs = sacarRepetidos xs
 listadeIds :: [Usuario] -> [Integer]
 listadeIds [] = []
 listadeIds (x:xs) = idDeUsuario x : listadeIds xs
+
 noHayIdsRepetidosaux :: [Integer] -> Bool
 noHayIdsRepetidosaux (x:xs) = sinRepetidos (x:xs)
 
