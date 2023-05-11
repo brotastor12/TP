c35b79d5d2fa346d961d70c8559a8e21576aeac5
brotastor12 <49822601+brotastor12@users.noreply.github.com>
2023-05-11 14:01:28 -0300

    Update TP.hs

--- TP.hs
+++ TP.hs
@@ -1,41 +1,31 @@
-type Usuario = (Integer, String) -- (id, nombre)
-type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
-type Publicacion = (Usuario, String, [Usuario]) -- (usuario que publica, texto publicacion, likes)
-type RedSocial = ([Usuario], [Relacion], [Publicacion])
+listadeIds :: [Usuario] -> [Integer]
+listadeIds [] = []
+listadeIds (x:xs) = idDeUsuario x : listadeIds xs
 
--- Funciones basicas
+noHayIdsRepetidosaux :: [Integer] -> Bool
+noHayIdsRepetidosaux (x:xs) = sinRepetidos (x:xs)
 
-usuarios :: RedSocial -> [Usuario]
-usuarios (us, _, _) = us
+noHayIdsRepetidos :: [Usuario] -> Bool
+noHayIdsRepetidos (x:xs) = noHayIdsRepetidosaux (listadeIds (x:xs))
 
-relaciones :: RedSocial -> [Relacion]
-relaciones (_, rs, _) = rs
+noHayPublicacionesRepetidas :: [Publicacion] -> Bool
+noHayPublicacionesRepetidas pubs = idsListasRepetidos (listaDeLikes pubs) && noHayIdsRepetidos  (listaDeUsuariosPubs pubs)
 
-publicaciones :: RedSocial -> [Publicacion]
-publicaciones (_, _, ps) = ps
+idsListasRepetidos :: [[Usuario]] -> Bool
+idsListasRepetidos (x:xs) = noHayIdsRepetidos x && idsListasRepetidos xs
 
-idDeUsuario :: Usuario -> Integer
-idDeUsuario (id, _) = id 
+listaDeLikes :: [Publicacion] -> [[Usuario]]
+listaDeLikes [] = []
+listaDeLikes (x:xs) = likesDePublicacion x : listaDeLikes xs
 
-nombreDeUsuario :: Usuario -> String
-nombreDeUsuario (_, nombre) = nombre 
+listaDeUsuariosPubs :: [Publicacion] -> [Usuario]
+listaDeUsuariosPubs [] = []
+listaDeUsuariosPubs (x:xs) = usuarioDePublicacion x : listaDeUsuariosPubs xs
 
-usuarioDePublicacion :: Publicacion -> Usuario
-usuarioDePublicacion (u, _, _) = u
 
-likesDePublicacion :: Publicacion -> [Usuario]
-likesDePublicacion (_, _, us) = us
+publicacionesValidas :: [Usuario] -> [Publicacion] -> Bool
+publicacionesValidas us pubs = incluido (listaDeUsuariosPubs pubs) us && likeDePublicacionSonUsuariosDeRed us pubs && noHayPublicacionesRepetidas pubs
 
--- Predicados 
 
-pertenece :: Eq t => t -> [t] -> Bool
-pertenece x ys 
-        | ys == [] = False
-        | x == head ys = True 
-        | x /= head ys = (pertenece x (tail ys)) 
-        
-mismoselementos :: Eq t => [t] -> [t] -> Bool
-mismoselementos xs ys 
-                | pertenece (head xs) ys == True && tail xs == [] = True
-                | pertenece (head xs) ys == False && tail xs == [] = False
-                | otherwise = (mismoselementos (tail xs) ys) && (pertenece (head xs) ys)
+likeDePublicacionSonUsuariosDeRed :: [Usuario] -> [Publicacion] -> Bool
+likeDePublicacionSonUsuariosDeRed us ((u,pub, likes) : pubs) = incluido likes us && likeDePublicacionSonUsuariosDeRed us pubs
