932c2d02390092df7cee6a963f26d70eb62d7d60
ahri10 <87216166+ahri10@users.noreply.github.com>
2023-05-19 19:41:17 -0300

    Update TP.hs

--- TP.hs
+++ TP.hs
@@ -2,7 +2,7 @@ module Solucion where
 
 -- Nombre de Grupo: xx
 -- Integrante 1: Astor Martinez Belizzi, astormbelizzi@gmail.com, 781/21
--- Integrante 2: Nombre Apellido, email, LU
+-- Integrante 2: Gonzalo Nicolas Moreira Valdez, gn.moreiravaldez@gmail.com, 712/23
 -- Integrante 3: Iván Alejandro Miguel Viola, ivanmiguelviola@gmail.com, 711/23
 -- Integrante 4: Nombre Apellido, email, LU
 
